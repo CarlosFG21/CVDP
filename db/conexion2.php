4c16b66e7b40ccb2316e356016e66c927daffcc7
marlonE7 <mat1.6@outlook.com>
2024-03-29 19:59:26 -0600

    actualizacion1

--- db/conexion2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conexion = mysqli_connect('localhost','root','','cvdp');
-    if(mysqli_connect_error())
-    {
-        die('Error no se pudo conectar a la base de datos'. mysqli_connect_error());
-    }
-?>
